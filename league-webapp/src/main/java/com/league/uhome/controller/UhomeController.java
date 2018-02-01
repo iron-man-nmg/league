f2c186c4adf7f5be141b73a0cab44cb0a364d874
wanglei <wanglei@wdai.com>
2018-02-01 21:09:34 +0800

    用户管理后台，

---
+++ league-webapp/src/main/java/com/league/uhome/controller/UhomeController.java
@@ -0,0 +1,59 @@
+package com.league.uhome.controller;
+
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.subject.Subject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.league.common.annotation.Log;
+import com.league.common.controller.BaseController;
+import com.league.common.domain.FileDO;
+import com.league.common.domain.Tree;
+import com.league.common.service.FileService;
+import com.league.common.utils.MD5Utils;
+import com.league.common.utils.R;
+import com.league.common.utils.ShiroUtils;
+import com.league.system.domain.MenuDO;
+import com.league.system.service.MenuService;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/uhome")
+public class UhomeController extends BaseController {
+	private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+	@Autowired
+	MenuService menuService;
+	@Autowired
+	FileService fileService;
+
+	@Log("请求访问主页")
+	@GetMapping({ "/","" })
+	String index(Model model) {
+		List<Tree<MenuDO>> menus = menuService.listMenuTree(getUserId());
+		model.addAttribute("menus", menus);
+		model.addAttribute("name", getUser().getName());
+		FileDO fileDO = fileService.get(getUser().getPicId());
+		if(fileDO!=null&&fileDO.getUrl()!=null){
+			if(fileService.isExist(fileDO.getUrl())){
+				model.addAttribute("picUrl",fileDO.getUrl());
+			}else {
+				model.addAttribute("picUrl","/img/photo_s.jpg");
+			}
+		}else {
+			model.addAttribute("picUrl","/img/photo_s.jpg");
+		}
+		model.addAttribute("username", getUser().getUsername());
+		return "index_uhome";
+	}
+}
