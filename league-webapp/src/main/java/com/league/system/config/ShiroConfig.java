5918149f63aa03135ed22016a08b81ee4a58b23e
wanglei <wanglei@wdai.com>
2018-02-02 15:04:47 +0800

    @todo

--- league-webapp/src/main/java/com/league/system/config/ShiroConfig.java
+++ league-webapp/src/main/java/com/league/system/config/ShiroConfig.java
@@ -80,6 +80,8 @@ public class ShiroConfig {
         filterChainDefinitionMap.put("/druid/**", "anon");
         filterChainDefinitionMap.put("/upload/**", "anon");
         filterChainDefinitionMap.put("/files/**", "anon");
+        filterChainDefinitionMap.put("/home/**", "anon");
+        filterChainDefinitionMap.put("/shop/**", "anon");
         filterChainDefinitionMap.put("/", "anon");
         filterChainDefinitionMap.put("/blog", "anon");
         filterChainDefinitionMap.put("/blog/open/**", "anon");
